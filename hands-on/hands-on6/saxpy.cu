24388dcc0a3419a9a6f73929fac3a83474870918
RobertoSSantos <rssan03@gmail.com>
2023-10-17 15:31:09 -0300

    hands-on 6 Added

---
+++ hands-on/hands-on6/saxpy.cu
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+
+__global__ void saxpy(int n,  float *x, float *y)
+{
+ int i = threadIdx.x;
+ if(i < n)
+  y[i] = x[i] + y[i];
+}
+
+void printVector(float *vector, int n)
+{
+
+ for (int i=0; i < n ; ++i)
+  printf("%1.0f\t", vector[i]);
+
+  printf("\n\n");
+}
+
+void generateVector(float *vector, int n)
+{
+ for (int i=0; i < n ; ++i)
+  vector[i] = i + 1;
+}
+
+int main(int argc, char *argv[])
+{
+  int n = atoi(argv[1]);   
+  float *x,*y;
+  float *xd, *yd;
+
+  x = (float*) malloc(sizeof(float) * n);
+  y = (float*) malloc(sizeof(float) * n);
+
+  cudaMalloc( (void**)&xd, sizeof(float) * n);
+  cudaMalloc( (void**)&xd, sizeof(float) * n);
+
+  cudaMemcpy(xd, x, sizeof(float) * n, cudaMemcpyHostToDevice);
+  cudaMemcpy(yd, y, sizeof(float) * n, cudaMemcpyHostToDevice);
+
+  generateVector(x, n);
+  printVector(x, n);
+
+  generateVector(y, n);
+  printVector(y, n);
+
+  int NUMBER_OF_BLOCKS = 1;
+  int NUMBER_OF_THREADS_PER_BLOCK = n;
+
+  saxpy<<<NUMBER_OF_BLOCKS, NUMBER_OF_THREADS_PER_BLOCK>>>(n,xd,yd);
+
+  cudaDeviceSynchronize();
+
+  cudaMemcpy(y, yd, sizeof(float) * n, cudaMemcpyDeviceToHost);
+
+  printVector(y, n);
+
+  cudaFree(xd);
+  cudaFree(yd);
+ 
+  free(x);
+  free(y);
+
+  return 0;
+
+}
